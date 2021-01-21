9b87514fa705d59964f14bc1f6558802d9addc9d
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2021-01-21 15:50:44 +0500

    3. LazyInitializationexception[331987#357494] (упражнение)

---
+++ src/main/java/lazy/HbmRunFill.java
@@ -0,0 +1,65 @@
+package lazy;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbmRunFill {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Category one = Category.of("First");
+            session.save(one);
+
+            Category two = Category.of("Second");
+            session.save(two);
+
+            Category three = Category.of("Another");
+            session.save(three);
+
+            Task t1 = Task.of("Task 1", one);
+            session.save(t1);
+
+            Task t2 = Task.of("Task 2", one);
+            session.save(t2);
+
+            Task t3 = Task.of("Task 3", one);
+            session.save(t3);
+
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static <T> T create(T model, SessionFactory sf) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(model);
+        session.getTransaction().commit();
+        session.close();
+        return model;
+    }
+
+    public static <T> List<T> findAll(Class<T> cl, SessionFactory sf) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<T> list = session.createQuery("from " + cl.getName(), cl).list();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
+}
